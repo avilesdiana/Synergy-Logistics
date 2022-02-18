3b82d1f1ff1ed6945a7ecb8e2dafe3ccb8a1b6fa
Diana <avilesdiana@outlook.com>
2022-02-18 05:12:33 -0600

    Prueba

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Estructura del proyecto Final Parte 2 Análisis")
+
+#Importar el csv
+import csv
+
+with open('synergy_logistics_database.csv','r') as archivo_csv:
+    lector = csv.reader(archivo_csv)
+
+    for linea in lector:
+        print(linea[2])
